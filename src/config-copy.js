76a4ccc598933cac585d57120718b80b26c48c3e
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2025-01-23 21:29:06 -0600

    Update

--- src/config-copy.js
+++ src/config-copy.js
@@ -4,3 +4,13 @@ var content = "6a2f41a3-c54c-fce8-32d2-0324e1c32e22";
 
 REVIEW
 //REVIEW
+
+function whoCalled() {
+   if (arguments.caller == null)   //Noncompliant
+      console.log('I was called from the global scope.');
+   else
+      console.log(arguments.caller + ' called me!');  // Noncompliant
+
+  console.log(whoCalled.caller);  // Noncompliant
+  console.log(whoCalled.arguments);  // Noncompliant
+}
